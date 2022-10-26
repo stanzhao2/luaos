07811ba80f7cfd4c6511f79f4e5eb3f4069e6473
stanzhao <2564643@qq.com>
2022-10-26 12:12:18 +0800

    update

--- libs/src/lua-socket/ndk/jni/Android.mk
+++ libs/src/lua-socket/ndk/jni/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE     := libsocket.io
 LOCAL_SRC_FILES  := ../../src/libsocket.io.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../include $(LOCAL_PATH)/../../../../3rd/asio
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../../include $(LOCAL_PATH)/../../../../../3rd/asio
 
 include $(BUILD_SHARED_LIBRARY)
