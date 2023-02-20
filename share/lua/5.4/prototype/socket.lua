7f040720f703f646e055b96e45d7fb11195d61e4
stanzhao2 <34759974+stanzhao2@users.noreply.github.com>
2023-02-20 09:43:25 +0800

    update

--- share/lua/5.4/prototype/socket.lua
+++ share/lua/5.4/prototype/socket.lua
@@ -115,9 +115,11 @@ function i_socket:receive(size) end;
 function i_socket:receive_from(size) end;
 
 ---开启 socket SSL 功能
----@param ctx ssl-context
+---@param  ctx ssl-context
+---@param  callback fun(status:boolean, subject:string)|nil
+---@param  certfile string|nil
 ---@return boolean
-function i_socket:sslv23(ctx) end;
+function i_socket:sslv23(ctx, callback, certfile) end;
 
 ---SSL 握手
 ---@overload fun(handler:fun(ec:integer):void):boolean
